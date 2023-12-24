d48f53218d461c9b08e8f7e8609ba006949d0caa
ArmanShlhn <armanshlhn@gmail.com>
2023-12-24 15:32:23 +0700

    hhh

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import torch
 import io
 import json
 
-
 # Load YOLOv5 model
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:/Kuliah/model/SDAA/model/best.pt')
 
@@ -34,9 +33,18 @@ def predict_image(file):
     }
 
 # Melakukan iterasi pada objek pred_img.xyxy[0]
+    detections_count = len(pred_img.xyxy[0])
+    class_count = len(class_mapping)
+
+    # Inisialisasi dictionary untuk menyimpan jumlah deteksi setiap kelas
+    class_detection_count = {class_id: 0 for class_id in class_mapping}
+
     for det in pred_img.xyxy[0]:
         # Format det: [x_min, y_min, x_max, y_max, confidence, class]
         x_min, y_min, x_max, y_max, _, class_id = map(int, det)
+
+        # Increment count untuk kelas ini
+        class_detection_count[class_id] += 1
 
         # Draw bounding box
         draw.rectangle([x_min, y_min, x_max, y_max], outline="red", width=2)
@@ -48,55 +56,15 @@ def predict_image(file):
         label = f"Class: {class_name}"
         draw.text((x_min, y_min - 10), label, fill="red")
 
+    # Menghitung persentase deteksi untuk setiap kelas
+    class_percentage = {class_id: (count / detections_count) * 100 for class_id, count in class_detection_count.items()}
+
+    # Menampilkan persentase deteksi untuk setiap kelas
+    for class_id, percentage in class_percentage.items():
+        class_name = class_mapping.get(class_id, f"Unknown Class {class_id}")
+        st.write(f"{class_name}: {percentage:.2f}%")
         
     return img_with_boxes
-
-
-
-def preprocess_image(image):
-    # Auto-orient the image
-    image = transforms.functional.autocontrast(image)
-    
-    # Resize the image to 640x640
-    transform = transforms.Compose([
-        transforms.Resize((640, 640)),
-        transforms.ToTensor(),
-    ])
-    
-    img_tensor = transform(image).float()
-    img_tensor /= 255.0  # Normalization (adjust as needed)
-    
-    return img_tensor.unsqueeze(0)  # Add batch dimension
-
-# Fungsi untuk melakukan deteksi objek menggunakan YOLOv5
-def perform_object_detection(image):
-    # Simpan gambar ke file untuk pengolahan oleh YOLOv5
-    image_path = "uploaded_image.jpg"
-    image.save(image_path)
-
-    # Load model YOLOv5 yang telah Anda latih
-    model_path = "model/best.pt"  # Sesuaikan dengan lokasi Anda
-
-    # Check if the model file exists
-    if not os.path.isfile(model_path):
-        st.error(f"Model file not found at path: {model_path}")
-        return None  # Return early if the model file is not found
-
-    model = torch.hub.load('ultralytics/yolov5:master', 'custom', path=model_path, force_reload=True)
-    model.eval()
-
-    # Load image for object detection
-    img_tensor = preprocess_image(image)
-
-    # Lakukan inferensi
-    with torch.no_grad():
-        results = model(img_tensor)
-
-    # Kembalikan hasil deteksi
-    return results
-
-# ... (fungsi lainnya tetap sama)
-
 
 
 def page_scanner():
@@ -107,7 +75,7 @@ def page_scanner():
     if uploaded_file is not None:
         st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
         st.write("")
-        st.write("Classifying...")
+        st.write("Cervical cancer cell screening results by VIUME")
 
         # Perform prediction
         pred_image = predict_image(uploaded_file)
@@ -144,8 +112,8 @@ pages = {
 
 # Layout navbar
 logo_path = "img\logoviume-removebg-preview.png"
-st.sidebar.image(logo_path, width=150)
-selected_page = st.sidebar.radio("Menu", list(pages.keys()))
+st.sidebar.image(logo_path, width=250)
+selected_page = st.sidebar.selectbox("", list(pages.keys()))
 
 # Memanggil fungsi halaman yang dipilih
 pages[selected_page]()
--- ori.py
+++ ori.py
@@ -52,3 +52,48 @@ selected_page = st.sidebar.radio("Menu", list(pages.keys()))
 
 # Memanggil fungsi halaman yang dipilih
 pages[selected_page]()
+
+
+def preprocess_image(image):
+    # Auto-orient the image
+    image = transforms.functional.autocontrast(image)
+    
+    # Resize the image to 640x640
+    transform = transforms.Compose([
+        transforms.Resize((640, 640)),
+        transforms.ToTensor(),
+    ])
+    
+    img_tensor = transform(image).float()
+    img_tensor /= 255.0  # Normalization (adjust as needed)
+    
+    return img_tensor.unsqueeze(0)  # Add batch dimension
+
+# Fungsi untuk melakukan deteksi objek menggunakan YOLOv5
+def perform_object_detection(image):
+    # Simpan gambar ke file untuk pengolahan oleh YOLOv5
+    image_path = "uploaded_image.jpg"
+    image.save(image_path)
+
+    # Load model YOLOv5 yang telah Anda latih
+    model_path = "model/best.pt"  # Sesuaikan dengan lokasi Anda
+
+    # Check if the model file exists
+    if not os.path.isfile(model_path):
+        st.error(f"Model file not found at path: {model_path}")
+        return None  # Return early if the model file is not found
+
+    model = torch.hub.load('ultralytics/yolov5:master', 'custom', path=model_path, force_reload=True)
+    model.eval()
+
+    # Load image for object detection
+    img_tensor = preprocess_image(image)
+
+    # Lakukan inferensi
+    with torch.no_grad():
+        results = model(img_tensor)
+
+    # Kembalikan hasil deteksi
+    return results
+
+# ... (fungsi lainnya tetap sama)
